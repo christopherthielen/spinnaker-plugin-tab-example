a89f9e0f26df3b0614d31363c3043792b586b4bb
Chris Thielen <github@sandgnat.com>
2021-05-12 15:16:24 -1000

    feat: add Data Source and Example State
    also remove WidgetizeStage

--- example-deck/src/index.ts
+++ example-deck/src/index.ts
@@ -1,6 +1,40 @@
-import { IDeckPlugin } from '@spinnaker/core';
-import { widgetizeStage } from './WidgetizeStage';
+import {
+  Application,
+  ApplicationDataSourceRegistry,
+  ApplicationStateProvider,
+  IDeckPlugin,
+  REST,
+} from '@spinnaker/core';
+
+import { ExampleView } from './ExampleView';
 
 export const plugin: IDeckPlugin = {
-  stages: [widgetizeStage],
+  initialize(plugin: IDeckPlugin) {
+    const injector = (window as any).spinnaker.$injector;
+    const applicationState: ApplicationStateProvider = injector.get('applicationState');
+    applicationState.addChildState({
+      name: 'example',
+      url: '/example',
+      views: {
+        insight: {
+          component: ExampleView,
+          $type: 'react',
+        },
+      },
+    });
+
+    ApplicationDataSourceRegistry.registerDataSource({
+      key: 'example',
+      label: 'Example',
+      autoActivate: true,
+      activeState: '**.example.**',
+      visible: true,
+      sref: '.example',
+      defaultData: [],
+      description: 'Example Data Source',
+      iconName: 'artifact',
+      loader: (application: Application) => REST('/example/data').get(),
+      onLoad: (application: Application, data: any) => Promise.resolve(data),
+    });
+  },
 };
